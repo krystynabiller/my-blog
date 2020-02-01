1ce29f5c22a5a768eec0e42a0b48795934cc35a6
Kristina <krystynabiller@Krystynas-MBP-2.lan>
2020-02-01 23:08:19 +0000

    Proud Immigrant Blog not fully working

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('about', views.about_me, name ='about_me'),
+    path('contact', views.contact, name ='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
